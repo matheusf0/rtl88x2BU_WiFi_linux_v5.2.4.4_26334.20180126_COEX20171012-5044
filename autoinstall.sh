ffd71dd9afe1bf77b6796495e91d14923a24b39b
Matheus Fornazieri <47992677+matheusf0@users.noreply.github.com>
2019-10-11 15:42:05 -0300

    auto install script

---
+++ autoinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VER=$(cat ./version)
+sudo rsync -rvhP ./ /usr/src/rtl88x2bu-${VER}
+sudo dkms add -m rtl88x2bu -v ${VER}
+sudo dkms build -m rtl88x2bu -v ${VER}
+sudo dkms install -m rtl88x2bu -v ${VER}
+sudo modprobe 88x2bu
